70b9798fc5e3f04305f6ff4c80ec38b4fe9b9e67
Deepayan Nandy <deepayan.622@gmail.com>
2022-07-05 05:01:04 +0530

    debug off

--- main.py
+++ main.py
@@ -32,4 +32,4 @@ def wo(wonum):
         return render_template('wo.html', wo=wodetails)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=False, host='0.0.0.0', port=80)

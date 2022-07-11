893a0d4210d86801e369c64636367a9171ee444b
Deepayan Nandy <deepayan.622@gmail.com>
2022-07-11 09:33:33 +0530

    v1.1

--- main.py
+++ main.py
@@ -18,6 +18,24 @@ def employee(emp):
         timecard= otsdb.getEmpTimesheet(user["uid"])
         print(timecard)
         return render_template('employee.html',user=user,card=timecard)
+@app.route('/eq/<eqid>')
+def eqp(eqid):
+    print(eqid)
+    res = otsdb.db.collection("equipments").where("eqId", "==", eqid).get()
+    if len(res)==0:
+        return render_template('DataNotFound.html', key="Equipment data not available")
+    else:
+        eq = res[0].to_dict()
+        return render_template('EQP.html',eq=eq)
+@app.route('/con/<conid>')
+def con(conid):
+    print(conid)
+    res = otsdb.db.collection("consumableItems").where("conId", "==", conid).get()
+    if len(res)==0:
+        return render_template('DataNotFound.html', key="Equipment data not available")
+    else:
+        con = res[0].to_dict()
+        return render_template('CON.html',con=con)
 @app.route('/po/<ponum>')
 def po(ponum):
     print(ponum)
@@ -38,4 +56,4 @@ def wo(wonum):
 if __name__ == '__main__':
     from waitress import serve
     # app.run(debug=False, host='0.0.0.0', port=80)
-    serve(app, host="0.0.0.0", port=80)
+    serve(app, host="0.0.0.0", port=8080)
